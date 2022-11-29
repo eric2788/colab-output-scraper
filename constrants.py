a317b00158311eb5f0266e3a4a516db94186c20c
Eric Lam <tsukiko852@gmail.com>
2022-11-29 09:27:03 +0000

    added disable-dev-shm-usage

--- constrants.py
+++ constrants.py
@@ -11,6 +11,8 @@ NOTEBOOK_URL = f'https://colab.research.google.com/drive/{os.getenv("NOTEBOOK_ID
 EMAIL = os.getenv('GMAIL')
 PASSWORD = os.getenv('GMAIL_PASSWORD')
 
+DISABLE_DEV_SHM = os.getenv('DISABLE_DEV_SHM', 'false').lower() == 'true'
+
 DATA_DIR = 'profile'
 COOKIE_PATH = f'{DATA_DIR}/cookies.json'
 
--- webdriver.py
+++ webdriver.py
@@ -3,6 +3,7 @@ import undetected_chromedriver.v2 as uc
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import Chrome
+from constrants import DISABLE_DEV_SHM
 
 def init_driver() -> Chrome:
     caps = DesiredCapabilities.CHROME.copy()
@@ -13,11 +14,12 @@ def init_driver() -> Chrome:
     options.add_argument("--disable-gpu")
 
     driver_path = None
-    
 
     if platform.startswith("linux"):
         driver_path = '/usr/bin/chromedriver'
         options.add_argument("--no-sandbox") # linux only
+        if DISABLE_DEV_SHM:
+            options.add_argument('--disable-dev-shm-usage') 
     driver: Chrome = uc.Chrome(desired_capabilities=caps, options=options, driver_executable_path=driver_path)
     driver.implicitly_wait(30)
     return driver
